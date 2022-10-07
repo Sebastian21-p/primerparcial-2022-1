7a98f19fe067d60d44c18f4745db6413e73e05d2
unknown <jesussebastianbber@ufps.edu.co>
2022-10-07 12:07:56 -0500

    intento de index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,36 @@
-function fillTable(data){
-    var table = document.querySelector("card");
-
-    for (let json in data){
-        
-   
-
-   let object = data[json];
-   let strDrink = object.strDrink;
-   let strIngredient1 = object.strIngredient1;
-   
-    
-    let row = card.insertRow();
-
-        let cell_index = row.insertCell();
-        cell_index.innerHTML = json;
-
-   var cell_cm = row.insertCell();
-   cell_strDrink.innerHTML =  object.strDrink;
-
-   var cell_edad = row.insertCell();
-   cell_strIngredient1.innerHTML =  object.strIngredient1;
-
-        
-}
-}
-
-
 function getData(){
-fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-.then(response => response.json())
-.then(data => fillTable(data));
-}
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(response => response.json())
+    .then(data => leer(data));
+    alert("Hola");
+    }
 
+    function leer(data){        
+        document.getElementById("cocteles").innerHTML =""
+        for (let i = 0; i < 3; i++){
+            let nombre = data.drinks[i].strDrink;
+            let ingrediente1 = data.drinks[i].strIngredient1
+            let ingrediente2 = data.drinks[i].strIngredient2
+            let ingrediente3 = data.drinks[i].strIngredient3
+            let alcoholic = data.drinks[i].strAlcoholic
+            let category = data.drinks[i].strCategory
 
-    getData();  
+            document.getElementById("cocteles").innerHTML +=`
+            <div class="card" style="width: 20rem;">
+            <img src="img/coctel.jpg" class="card-img-top" alt="..." id="img">
+            <div class="card-body">
+              <h5 class="card-title">${nombre}</h5>
+              <h6>Ingredientes</h6>
+              <p class="card-text" id="ingredientes">${ingrediente1}, ${ingrediente2}, ${ingrediente3}</p>
+              <h2>${alcoholic}</h2>
+              <a href="#" class="btn btn-primary" id="comprar">Comprar</a>
+            </div>
+          </div>    
+            `
+            
+    
+        }
 
-//getData();
+        
+    }
+    leer();
